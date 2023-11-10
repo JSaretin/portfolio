bb88cb868bf62e389d73beba33528faf8ab2b58f
JSaretin <jsaretin@gmail.com>
2023-11-10 15:02:13 +0100

    updated nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,19 +7,27 @@
 </script>
 
 <div class="relative w-full font-Source">
-	<div class="static w-full left-0 top-0 shadow-sm shadow-gray-200">
-		<div class="max-w-4xl w-full mx-auto flex justify-between text-white p-4">
-			<div class="flex-1"><h1 class="font-bold uppercase">JSaretin</h1></div>
-			<ul class="flex justify-between flex-1 text-sm text-gray-200">
+	<div
+		class="fixed bg-black bg-opacity-70 backdrop-blur-sm lg:static w-full left-0 top-0 h-screen lg:h-fit lg:shadow-sm lg:shadow-gray-200 flex lg:block justify-center align-middle place-items-center translate-x-full lg:translate-x-0 transition-transform duration-200 ease-in-out"
+	>
+		<div
+			class="h-full max-w-4xl lg:w-full mx-auto flex flex-col lg:flex-row justify-between text-white p-4"
+		>
+			<div class="hidden lg:block flex-1"><h1 class="font-bold uppercase">JSaretin</h1></div>
+			<ul
+				class="flex flex-col lg:flex-row justify-between flex-1 text-3xl lg:text-sm text-gray-200"
+			>
 				<li><a href="/">Home</a></li>
 				<li><a href="/">About</a></li>
 				<li><a href="/">Skill</a></li>
 				<li><a href="/">Projects</a></li>
 				<li><a href="/">Socials</a></li>
 			</ul>
-			<ul class="flex-1 flex justify-end">
-				<li>
+			<ul class="flex-1 flex flex-col lg:flex-row justify-end gap-4">
+				<li class="">
 					<button class="bg-blue-500 p-1 rounded text-gray-950 text-sm">Get In Touch</button>
+				</li>
+				<li class="">
 					<button class="bg-green-400 p-1 rounded text-gray-950 text-sm">Get In Quote</button>
 				</li>
 			</ul>
